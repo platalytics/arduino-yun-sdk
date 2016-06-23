7049159d41bce83038f96452647614ef3f2ea6f2
mfahadsid <mfahadsiddiqui1@hotmail.com>
2016-06-23 10:25:02 +0500

    getopts added

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
 
-if [ $# -ne 6 ]; then
-    echo "usage: ./deploy.sh <board-ip> <board-username> <board-password> <ssh-port> <device-key> <frontend-host>"
+if [ $# -ne 12 ]; then
+    echo """Usage: ./deploy.sh -<option1> <value1> ...
+    -i IP address
+    -u username having root access
+    -p password of specified username
+    -s SSH port
+    -d device ID/key generated from Platalytics Platform
+    -f frontend host"
+
+    echo "Example: ./deploy -i 192.168.1.X -u root -p arduino -s 22 -d KEY -f http://host:port/xxx/xxx?api_key=xxx"
+
 else
-    board_ip=$1
-    board_username=$2
-    board_password=$3
-    ssh_port=$4
-    device_id=$5
-    frontend_host=$6
+    while getopts ":i:u:p:s:d:f:" opt; do
+        case $opt in
+            i)
+                board_ip=$OPTARG
+                ;;
+            u)
+                board_username=$OPTARG
+                ;;
+            p)
+                board_password=$OPTARG
+                ;;
+            s)
+                ssh_port=$OPTARG
+                ;;
+            d)
+                device_id=$OPTARG
+                ;;
+            f)
+                frontend_host=$OPTARG
+                ;;
+            \?)
+                echo "Invalid flag: -$OPTARG." ; exit 1
+                ;;
+            :)
+                echo "Option -$OPTARG requires an argument."
+                ;;
+        esac
+    done
 
     # defining paths
     libraries="./lib"
